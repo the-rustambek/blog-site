ff560e503ddefb1e18db704425a38fe8a248badf
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2021-09-13 12:37:19 +0500

    add jwt

---
+++ modules/jwt.js
@@ -0,0 +1,23 @@
+const {sign, verify} = require("jsonwebtoken");
+
+const SECRET_WORD =  process.env,SECRET_WORD;
+
+async function createToken(data){
+    return sign(data, SECRET_WORD,{
+        expiresIn:"7d",
+    });
+}
+
+async function validateToken (token){
+    try{
+        return verify(token, SECRET_WORD);
+    }
+    catch(error){
+        return false;
+    }
+}
+
+module.exports = {
+    validateToken,
+    createToken,
+}
