910754f5471229022cd6a22963add002bc84d7ba
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2021-09-13 13:20:10 +0500

    add  val

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -6,7 +6,7 @@ router.get("/",(req,res) =>{
     res.render("index");
 });
 
-router.post("/registration",(req,res) =>{
+router.post("/register",async (req,res) =>{
     // console.log(req.body);
     try {
         const data = await SignUpValidation.validateAsync(req.body);
